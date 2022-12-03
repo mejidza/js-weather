adbd7c8e7c3c21be05467c0d1e46c734a646635f
mejidza <clackz2k16@gmail.com>
2022-12-04 02:45:55 +0300

    revorked app

--- script.js
+++ script.js
@@ -1,51 +1,31 @@
-   let cityName = document.getElementById("london")
-   cityName.addEventListener("click", () => { 
-      fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2e91651b68de96e0310715de0d64e9e9")
-      .then (function(resp){ return resp.json() })
-      .then (function(data) {
-          console.log(data)
-          document.querySelector(".city-name-text").textContent = data.name
-          document.querySelector(".price").innerHTML = Math.round(data.main.temp - 273) + "&deg;"
-          document.querySelector(".disclamer").textContent = data.weather[0]["description"]
-          document.querySelector(".city-name-text").textContent = data.name
-          document.querySelector(".features").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`
-          document.querySelector(".city-name-subtext").textContent = data.sys.country
+   let cityWeatherIcon = document.getElementById("weatherIcon")
+   let cityWeatherPrice = document.getElementById("cityWeatherInfo")
+   let cityWeatherInfo = document.getElementById("cityText")
+   let inputValue = ""
+   let cityFindInput = document.getElementById("cityFindInput")
+   cityFindInput.addEventListener("change", () => {
+      inputValue = cityFindInput.value
+   })
+   let cityFindButton = document.getElementById("addCityButton")
+   cityFindButton.addEventListener("click", () => {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=2e91651b68de96e0310715de0d64e9e9`)
+      .then (res => res.json())
+      .then (data => {
+         console.log(data)
+         cityWeatherInfo.innerText = data.name
+         cityWeatherPrice.innerText = farinToCelsies(data.main.temp)
+         cityWeatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
       })
-      .catch(function(){
-      }) 
+
    })
-   let secondCityName = document.getElementById("tokyo")
-   secondCityName.addEventListener("click", () => {
-      fetch("https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=2e91651b68de96e0310715de0d64e9e9")
-      .then (function(resp){ return resp.json() })
-      .then (function(data) {
-          console.log(data)
-          document.querySelector(".city-name-text").textContent = data.name
-          document.querySelector(".price").innerHTML = Math.round(data.main.temp - 273) + "&deg;"
-          document.querySelector(".disclamer").textContent = data.weather[0]["description"]
-          document.querySelector(".city-name-text").textContent = data.name
-          document.querySelector(".features").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`
-          document.querySelector(".city-name-subtext").textContent = data.sys.country
-         })
-      .catch(function(){
-      }) 
-   })
-   let thirdCityName = document.getElementById("moscow")
-   thirdCityName.addEventListener("click", () => { 
-      fetch("https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=2e91651b68de96e0310715de0d64e9e9")
-      .then (function(resp){ return resp.json() })
-      .then (function(data) {
-          console.log(data)
-          document.querySelector(".city-name-text").textContent = data.name
-          document.querySelector(".price").innerHTML = Math.round(data.main.temp - 273) + "&deg;"
-          document.querySelector(".disclamer").textContent = data.weather[0]["description"]
-          document.querySelector(".city-name-text").textContent = data.name
-          document.querySelector(".features").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`
-          document.querySelector(".city-name-subtext").textContent = data.sys.country
-         })
-      .catch(function(){
-      }) 
-   })
+   function farinToCelsies(temp) {
+      return (temp - 273.15).toFixed(1)
+   }
+
+
+
+
+   
   
  
 
